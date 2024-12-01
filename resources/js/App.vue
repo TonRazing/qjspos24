900362d8dcff50b36a0aad035093982142013ab5
TonRazing <tonbtbd@gmail.com>
2024-12-01 17:46:29 +0700

    first commit qjspos24 v1 20241201

---
+++ resources/js/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Ton Racing</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Qjspos24App',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="SCSS" scoped>
+
+</style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>HOME</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Qjspos24Home',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="SCSS" scoped>
+
+</style>
