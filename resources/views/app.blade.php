4e727ab4632b3d4ccce63f6b452e2a88d96861c5
TonRazing <tonbtbd@gmail.com>
2024-12-02 22:19:12 +0700

    Update Qjspos24 font-end 20241202

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,17 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ url('assets/vendor/css/core.css')}}" class="template-customizer-core-css">
+    <link rel="stylesheet" href="{{ url('assets/vendor/css/theme-default.css')}}" class="template-customizer-theme-css">
+    <link rel="stylesheet" href="{{ url('assets/css/demo.css')}}">
+
+    <!-- Boxicon -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
     @vite('resources/css/app.css')
 
 </head>
 <body>
-    <h1>Test Laravel version 15</h1>
+    
 
     <hr>
 
     <div id="app-vue">
         
     </div>
+
+  <script src="{{ url('assets/vendor/libs/jquery/jquery.js')}}"></script>
+  <script src="{{ url('assets/vendor/libs/popper/popper.js')}}"></script>
+  <script src="{{ url('assets/vendor/js/bootstrap.js')}}"></script>
+  <script src="{{ url('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+  <script src="{{ url('assets/vendor/libs/hammer/hammer.js')}}"></script>
+  <script src="{{ url('assets/vendor/libs/i18n/i18n.js')}}"></script>
+  <script src="{{ url('assets/vendor/libs/typeahead-js/typeahead.js')}}"></script>
+  <script src="{{ url('assets/vendor/js/menu.js')}}"></script>
 
     @vite('resources/js/app.js')
 </body>
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('{any}', function () {
     return view('app');
-});
+})->where('any','.*');
 
 // Route::get('/test', function () {
 //     return view('app');
